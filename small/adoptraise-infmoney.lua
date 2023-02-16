2e189e99b9525b29aaeb790f3eb1259e6182b7fe
Lava <69821200+RealLava@users.noreply.github.com>
2023-02-16 14:04:07 -0800

    Update adoptraise-infmoney.lua

--- small/adoptraise-infmoney.lua
+++ small/adoptraise-infmoney.lua
@@ -1,1 +1,9 @@
-
+game.ReplicatedStorage.money:FireServer(math.huge)
+-- https://www.roblox.com/games/6122000026/Family-Adopt-and-Raise
+-- https://www.roblox.com/games/8387752349/Adopt-and-Raise-a-Child
+-- https://www.roblox.com/games/5144978463/FAMILY-Raise-a-Family
+-- https://www.roblox.com/games/7385854324/Adopt-and-Raise-a-Cute-Baby
+-- https://www.roblox.com/games/6158952962/Adopt-and-Raise-a-Family
+-- https://www.roblox.com/games/12199032836/Adopt-and-Raise-a-Cute-Baby-Update-4
+-- https://www.roblox.com/games/8866839093/Adopt-and-Raise-a-Baby
+-- https://www.roblox.com/games/8390005819/Life-In-Paradise-CHRISTMAS-UPDATE
